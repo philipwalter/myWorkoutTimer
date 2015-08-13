fe691cfc17e65b069e90f3a841b27417639008a6
philipwalter <pdwalter@ualr.edu>
2015-08-13 16:44:50 -0500

    Moving toward object orientation
    Began instantiating Timers and started making JSDoc comment blocks

--- js/timerMain.js
+++ js/timerMain.js
@@ -1,8 +1,7 @@
-
+/** @namespace */
 window.MyModule = {};
 
-// Commence scope to prevent littering 
-// the window object with unwanted variables
+/** @class */
 window.MyModule.Timer = function() {
 
 	var min = 01, sec = 40, running = false;
@@ -35,8 +34,9 @@ window.MyModule.Timer = function() {
 			time = (min<=9 ? "0" + min : min) + " min and " + sec + " sec ";
 
 			if (document.getElementById) { document.getElementById('timerTest').innerHTML = time; }
-
-			SD=window.setTimeout("window.MyModule.Timer.countDown();", 1000);
+			
+			var _this = this;
+			SD=window.setTimeout(function() { _this.countDown(); }, 1000);
 			if (min == '00' && sec == '00') { sec = "00"; window.clearTimeout(SD); running = false;}
 		},
 		
@@ -52,10 +52,18 @@ window.MyModule.Timer = function() {
 		
 	};
 
-}();
+}
 
-window.MyModule.Timer.getMinutes();
-window.MyModule.Timer.setMinutes(10);
-window.MyModule.Timer.getMinutes();
-window.MyModule.Timer.countDown();
-window.setTimeout("window.MyModule.Timer.stop();", 5000);
+//window.MyModule.Timer.getMinutes();
+//window.MyModule.Timer.setMinutes(10);
+//window.MyModule.Timer.getMinutes();
+//window.MyModule.Timer.countDown();
+//window.setTimeout("window.MyModule.Timer.stop();", 5000);
+
+var clock2 = new window.MyModule.Timer;
+clock2.getMinutes();
+clock2.setMinutes(10);
+clock2.getMinutes();
+var clock3 = new window.MyModule.Timer;
+clock3.getMinutes();
+clock2.countDown();
